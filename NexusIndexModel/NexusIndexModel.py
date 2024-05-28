33a92d5a0e7a9016b49bd82a01d33e3dc3d85daf
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-05-27 20:32:32 -0700

    Create NexusIndexModel.py

---
+++ NexusIndexModel/NexusIndexModel.py
@@ -0,0 +1,13 @@
+class NexusIndexModel(nn.Module):
+    def __init__(self, input_size):
+        super(NexusIndexModel, self).__init__()
+        self.fc1 = nn.Linear(input_size, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.sigmoid(self.fc3(x))
+        return x
