45d557a50b45b17ec0f89d276fb518a6da53a284
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-05-27 20:38:51 -0700

    Create NexusIndexModel_train.py

---
+++ NexusIndex_PyTorch_tensors/NexusIndexModel_train/NexusIndexModel_train.py
@@ -0,0 +1,8 @@
+num_epochs = 1000
+for epoch in range(num_epochs):
+    model.train()
+    optimizer.zero_grad()
+    outputs = model(X_train_tensor)
+    loss = criterion(outputs, y_train_tensor)
+    loss.backward()
+    optimizer.step()
